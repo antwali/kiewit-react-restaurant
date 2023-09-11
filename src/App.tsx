27d0d2d3ccc2717d7cb5546add00334cacfc25c5
Alisson.Ntwali <98063586+antwali@users.noreply.github.com>
2023-09-11 13:20:25 -0600

    third commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
-import { Food, foods } from "./food";
+import { useState } from "react";
+import { Food, FoodTag, foodTags, foods } from "./food";
+import { Heading } from "./shared/Heading";
 
 export function App() {
+  const [tagFilter, setTagFilter] = useState<null | FoodTag>(null);
+
+  const filteredFoods = foods.filter((food) => {
+    return tagFilter ? food.tags.includes(tagFilter) : true;
+    // if (tagFilter === null) {
+    //   return true;
+    // }
+    // return food.tags.includes(tagFilter);
+  });
+
   function renderFood(food: Food) {
     return (
       <div key={food.id} className="m-4 p-2 bg-cyan-100 w-52 border shadow-md">
-        <h2 className="text-xl font-bold ">{food.name}</h2>
+        <Heading tag="h2">{food.name}</Heading>
+        <img
+          src={`images/${food.image}`}
+          alt={food.name}
+          className="max-h-36"
+        />
         <p>{food.description}</p>
         <p className="font-bold">{food.price}</p>
       </div>
@@ -13,8 +30,20 @@ export function App() {
 
   return (
     <>
-      <h1> Menu</h1>
-      <div className="flex flex-wrap">{foods.map(renderFood)}</div>
+      <Heading tag="h1"> Menu</Heading>
+      <label htmlFor="tag-filter">Filter by Tag</label>
+      <br />
+      <select
+        value={tagFilter ?? ""}
+        id="tag-filter"
+        onChange={(e) => setTagFilter(e.target.value as FoodTag)}
+      >
+        <option value="">All</option>
+        {foodTags.map((tag) => (
+          <option key={tag}>{tag}</option>
+        ))}
+      </select>
+      <div className="flex flex-wrap">{filteredFoods.map(renderFood)}</div>
     </>
   );
 }
